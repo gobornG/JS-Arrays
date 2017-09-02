ae5438f21ab207a496bd22ff06808c72ff4bff3e
HOME\Garry <gtoborn@hotmail.com>
2017-09-01 20:41:00 -0600

    finished array project

--- arraysPractice.js
+++ arraysPractice.js
@@ -40,10 +40,18 @@ function looper(family) {
 
 
 var letters = ['A', 'B', 'C', 'D', 'E'];
-//Write a function called reversedLooper that is given letters as it's only argument and loops through the given array backwards alerting every item in the array starting at the end.
+//Write a function called reversedLooper that is given letters as it's only argument and loops through the given array backwards
+//alerting every item in the array starting at the end.
 
 //Code Here
-
+function reversedLooper(letters) {
+    var reversedArr = letters.reverse();
+    for (var i = reversedArr.length - 1; i >= 0; i--) {
+      alert(reversedArr[i]);
+    }
+  }
+  
+  reversedLooper(letters);
 
 //Next Problem
 
@@ -52,7 +60,15 @@ var nums = [1, 2, 3, 6, 22, 98, 45, 23, 22, 12];
 //Write a function named evenFinder that is given nums as it's only argument and removes all values that aren't even from the given array.
 
 //Code Here
-
+function evenFinder(nums) {
+    var evenNums = [];
+    for (var i = 0; i < nums.length; i++) {
+        if (nums[i] % 2 === 0) {
+            evenNums.push(nums[i]);
+        }
+    }
+    return evenNums;
+}
 
 
 
